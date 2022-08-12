ac05199e151fa38609e2ba6be989c1edaa823af7
dabaozha <jsj987267351@163.com>
2022-08-12 16:59:07 +0800

    git demo2

--- Maven_demo/src/main/java/git/gitdemo.java
+++ Maven_demo/src/main/java/git/gitdemo.java
@@ -3,6 +3,8 @@ package git;
 public class gitdemo {
     public static void main(String[] args) {
 
-        System.out.println("Hello");
+        System.out.println("Hello1");
+        System.out.println("Hello2");
+        System.out.println("Hello3");
     }
 }
