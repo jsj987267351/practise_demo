d65c72e7a1da1334b564a12deb375d1269a1dea2
dabaozha <jsj987267351@163.com>
2022-08-12 17:17:24 +0800

    master commit

--- Alldemo/src/main/java/git/gitdemo.java
+++ Alldemo/src/main/java/git/gitdemo.java
@@ -2,6 +2,9 @@ package git;
 
 public class gitdemo {
     public static void main(String[] args) {
+
         System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello master");
     }
 }
