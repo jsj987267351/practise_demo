7d8e437988a760c94f0af8ee315e8ba58b29e691
dabaozha <jsj987267351@163.com>
2022-11-16 11:17:20 +0800

    2022.11.16 Git practise-final

--- practisegit/src/main/java/git.java
+++ practisegit/src/main/java/git.java
@@ -4,5 +4,6 @@ public class git {
         System.out.println("Git");
         System.out.println("Git from Github");
         System.out.println("Git from Gitee");
+        System.out.println("Git success");
     }
 }
