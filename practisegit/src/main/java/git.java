614aff515a8ec942ba80a11f71bf924cd59a024b
张朝证 <jsj987267351@163.com>
2022-11-16 03:16:15 +0000

    update practisegit/src/main/java/git.java.
    Signed-off-by: 张朝证 <jsj987267351@163.com>

--- practisegit/src/main/java/git.java
+++ practisegit/src/main/java/git.java
@@ -3,5 +3,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("Git");
         System.out.println("Git from Github");
+        System.out.println("Git from Gitee");
     }
 }
