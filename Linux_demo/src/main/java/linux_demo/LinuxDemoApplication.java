76f9d0c99b3fad1fd671250c99db67df6b7d2149
dabaozha <jsj987267351@163.com>
2023-02-04 15:35:29 +0800

    2023.2.4  linux

---
+++ Linux_demo/src/main/java/linux_demo/LinuxDemoApplication.java
@@ -0,0 +1,13 @@
+package linux_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinuxDemoApplication.class, args);
+    }
+
+}
