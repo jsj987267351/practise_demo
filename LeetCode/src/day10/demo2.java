c5855568cf8f60a4f1ccbb96c9f41be97762ac27
dabaozha <jsj987267351@163.com>
2023-03-26 17:02:42 +0800

    LeetCode 10

--- LeetCode/src/day10/demo2.java
+++ LeetCode/src/day10/demo2.java
@@ -7,21 +7,39 @@ package day10;
  * @date 2023/3/26 15:42
  */
 public class demo2 {
-    public void rotate(int[] nums, int k) {
+    public void rotate1(int[] nums, int k) {
         int[] result = new int[nums.length + k];
         for (int i = 0; i < nums.length; i++) {
             result[i + k] = nums[i];
         }
 
-        int i  = k-1;
-        int j  = result.length -1;
-        while (i>=0){
+        int i = k - 1;
+        int j = result.length - 1;
+        while (i >= 0) {
             result[i] = result[j];
             i--;
             j--;
         }
         for (int l = 0; l < nums.length; l++) {
             nums[l] = result[l];
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        int temp = 0;
+        while (start < end) {
+            temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
