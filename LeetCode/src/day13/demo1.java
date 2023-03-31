ecc96a2057609a9ffdc0b940f6f67e861c76833f
dabaozha <jsj987267351@163.com>
2023-03-31 15:53:12 +0800

    LeetCode 13

---
+++ LeetCode/src/day13/demo1.java
@@ -0,0 +1,38 @@
+package day13;
+
+import java.util.LinkedList;
+
+/**
+ * @author ：大爆炸
+ * @version 1.0
+ * @description 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * @date 2023/3/31 13:20
+ */
+public class demo1 {
+    public boolean isValid(String s) {
+        LinkedList<Character> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c =='(' ){
+                stack.push(')');
+            }else if (c =='['){
+                stack.push(']');
+            }else if (c == '{'){
+                stack.push('}');
+            }else {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                Character pop = stack.pop();
+                if (pop != c){
+                    return  false;
+                }
+            }
+        }
+        if (!stack.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+}
+
---
+++ LeetCode/src/day13/demo2.java
@@ -0,0 +1,48 @@
+package day13;
+
+/**
+ * @author ：大爆炸
+ * @version 1.0
+ * @description 给你一个下标从 1 开始的整数数组 numbers ，该数组已按 非递减顺序排列  ，请你从数组中找出满足相加之和等于目标数 target 的两个数
+ * @date 2023/3/31 14:07
+ */
+public class demo2 {
+    public int[] twoSum1(int[] numbers, int target) {
+        int[] result = new int[2];
+        for (int i = 1; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length + 1; j++) {
+                if (numbers[i - 1] + numbers[j - 1] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+
+    public int[] twoSum2(int[] numbers, int target) {
+        int[] result = new int[2];
+        int i = 0;
+        int j = numbers.length - 1;
+        while (i <= j) {
+            int m = (i + j) >>> 1;
+            if (numbers[i] + numbers[m] > target) {
+                j = m;
+            } else if (numbers[j] + numbers[m] < target) {
+                i = m;
+            } else if (numbers[i] + numbers[j] < target) {
+                i++;
+            } else if (numbers[i] + numbers[j] > target) {
+                j--;
+            } else {
+                result[0] = i + 1;
+                result[1] = j + 1;
+                break;
+            }
+        }
+        return result;
+    }
+}
+
