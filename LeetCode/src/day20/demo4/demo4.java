9b1456107eddc430f53422cf8c044b63ef5a2199
dabaozha <jsj987267351@163.com>
2023-04-10 17:16:59 +0800

    LeetCode 20

---
+++ LeetCode/src/day20/demo4/demo4.java
@@ -0,0 +1,32 @@
+package day20.demo4;
+
+import java.beans.beancontext.BeanContext;
+
+/**
+ * @author ：大爆炸
+ * @version 1.0
+ * @description TODO
+ * @date 2023/4/10 17:10
+ */
+public class demo4 {
+    public ListNode deleteNode(ListNode head, int val) {
+        if (head.val == val) {
+            return head.next;
+        }
+        ListNode a = new ListNode(-1);
+        a.next = head;
+        while (a.next != null) {
+            if (a.next.val == val && a.next.next != null) {
+                a.next = a.next.next;
+                break;
+            }
+            if (a.next.val == val && a.next.next == null) {
+                a.next = null;
+                break;
+            }
+            a = a.next;
+        }
+        return head;
+    }
+}
+
