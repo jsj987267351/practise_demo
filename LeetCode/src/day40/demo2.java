d07c5f78bf1a5c021a5c38c48f6859df6f717963
dabaozha <jsj987267351@163.com>
2023-05-11 16:58:45 +0800

    LeetCode 40

---
+++ LeetCode/src/day40/demo2.java
@@ -0,0 +1,36 @@
+package day40;
+
+/**
+ * @author ：大爆炸
+ * @version 1.0
+ * @description 给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
+ * <p>
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * <p>
+ * 判断你是否能够到达最后一个下标。
+ * https://leetcode.cn/problems/jump-game/
+ * @date 2023/5/11 15:56
+ */
+public class demo2 {
+    public static boolean canJump(int[] nums) {
+        if (nums.length == 1) {
+            return true;
+        }
+//        覆盖范围
+        int cover = 0;
+            for (int i = 0; i <= cover; i++) {
+//                cover改变时，for循环里也会改变，所以不需要用while循环
+                cover = Math.max(cover, i + nums[i]);
+                if (cover >= nums.length - 1)
+                    return true;
+            }
+        return false;
+    }
+
+    public static void main(String[] args) {
+         int[] a  = {2,3,1,1,4};
+        boolean b = canJump(a);
+        System.out.println(b);
+    }
+}
+
---
+++ LeetCode/src/day40/demo3.java
@@ -0,0 +1,70 @@
+package day40;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ：大爆炸
+ * @version 1.0
+ * @description 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ * https://leetcode.cn/problems/generate-parentheses/
+ * @date 2023/5/11 16:29
+ */
+public class demo3 {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+//        backtracking(result, stringBuilder, n, n);
+        DFS(result,"",n,n);
+        return result;
+    }
+
+
+    public void DFS(List<String> result, String string, int left, int right) {
+
+//        左右括号都使用完了
+        if (left == 0 && right == 0) {
+            result.add(string);
+            return;
+        }
+//        剪枝，左括号剩余数量应该严格小于等于右括号剩余数量
+        if (left > right) {
+            return;
+        }
+//        左括号还有剩余
+        if (left > 0) {
+            DFS(result,string+"(",left-1,right);
+        }
+//        右括号还有剩余
+        if (right > 0) {
+            DFS(result,string+")",left,right-1);
+        }
+    }
+
+
+    //    这里的left和right代表左右括号的剩余使用数目
+    public void backtracking(List<String> result, StringBuilder stringBuilder, int left, int right) {
+//        左右括号都使用完了
+        if (left == 0 && right == 0) {
+            result.add(stringBuilder.toString());
+            return;
+        }
+//        剪枝，左括号剩余数量应该严格小于等于右括号剩余数量
+        if (left > right) {
+            return;
+        }
+//        左括号还有剩余
+        if (left > 0) {
+            stringBuilder.append("(");
+            backtracking(result, stringBuilder, left - 1, right);
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        }
+//        右括号还有剩余
+        if (right > 0) {
+            stringBuilder.append(")");
+            backtracking(result, stringBuilder, left, right - 1);
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        }
+    }
+}
+
