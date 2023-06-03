88b4215d4001e3fdc596512f8caeff1ae0dd63a0
dabaozha <jsj987267351@163.com>
2023-06-03 21:28:37 +0800

    LeetCode 56

---
+++ LeetCode/src/day55/demo3/demo3.java
@@ -0,0 +1,39 @@
+package day55.demo3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ：大爆炸
+ * @version 1.0
+ * @description 输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
+ * <p>
+ * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ * https://leetcode.cn/problems/zhong-jian-er-cha-shu-lcof/
+ * @date 2023/6/2 20:03
+ */
+public class demo3 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0) {
+            return null;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return help(preorder, 0, preorder.length - 1, 0, inorder.length - 1, map);
+    }
+
+    public TreeNode help(int[] preorder, int preLeft, int preRight, int inLeft, int inRight, Map<Integer, Integer> map) {
+        if (preLeft > preRight || inLeft > inRight) {
+            return null;
+        }
+        int rootValue = preorder[preLeft];
+        TreeNode root = new TreeNode(rootValue);
+        Integer index = map.get(rootValue);
+        root.left = help(preorder, preLeft + 1, index - inLeft + preLeft, inLeft, index - 1, map);
+        root.right = help(preorder, index - inLeft + preLeft + 1, preRight, index + 1, inRight, map);
+        return root;
+    }
+}
+
---
+++ LeetCode/src/day56/demo2.java
@@ -0,0 +1,40 @@
+package day56;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ：大爆炸
+ * @version 1.0
+ * @description 无重复字符串的排列组合。编写一种方法，计算某字符串的所有排列组合，字符串每个字符均不相同。
+ * https://leetcode.cn/problems/permutation-i-lcci/
+ * @date 2023/6/3 21:17
+ */
+public class demo2 {
+    public String[] permutation(String S) {
+        char[] chars = S.toCharArray();
+        boolean[] used = new boolean[chars.length];
+        List<String> list = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        backtracking(chars, used, list, stringBuilder, chars.length);
+        return list.toArray(new String[list.size()]);
+
+    }
+
+    public void backtracking(char[] chars, boolean[] used, List<String> list, StringBuilder stringBuilder, int length) {
+        if (stringBuilder.length() == length) {
+            list.add(stringBuilder.toString());
+            return;
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (used[i]) continue;
+            stringBuilder.append(chars[i]);
+            used[i] = true;
+            backtracking(chars, used, list, stringBuilder, length);
+            used[i] = false;
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        }
+    }
+}
+
